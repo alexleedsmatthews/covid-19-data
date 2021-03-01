78097ce3691c9c6e70dc41efe6bbb204619f7f3c
Alex Leeds Matthews <alexleedsmatthews@gmail.com>
2021-02-28 22:22:04 -0500

    data updates

--- CaseCurves.R
+++ CaseCurves.R
@@ -6,8 +6,26 @@ require(tigris)
 require(sf)
 require(covid19nytimes)
 
-covid19nytimes_states <- refresh_covid19nytimes_states() %>%
-  filter(date == max(date)) 
+# start with national data
 
-covid19nytimes_counties <- refresh_covid19nytimes_counties()%>%
-  filter(date == max(date)) 
+# read it in and create a new cases column
+require(readr)
+national_data <- read_csv("us.csv", col_types = cols(date = col_date(format = "%Y-%m-%d")))
+View(us)
+
+national_data$new_cases <- diff(c(0,national_data$cases))
+
+# making a weekly average of daily new cases
+
+
+# plot it as a column chart
+cases_chart <-  ggplot(national data, aes(x = date, y = new_cases)) +
+  xlab("date") +
+  ylab("Daily cases")
+         
+
+# read states data
+
+us_states <- read_csv("us-states.csv", col_types = cols(date = col_date(format = "%Y-%m-%d")))
+View(us_states)
+
